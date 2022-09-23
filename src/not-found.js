a2ee87bd48267e513bec1dd9143137b5f5661ac0
Joseph Tobi <jtobi8161@gmail.com>
2022-09-23 15:19:44 +0100

    Delete all duplicate and unnecessary old frontend files

--- src/not-found.js
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-    return (
-        <div className="d-flex flex-column mh-100vh align-items-center justify-content-center ">
-            <h1 className="display-3" >404 page not found</h1>
-            <p className="p-3">OOPS, the page you are looking for doesn't exist.</p>
-        </div>
-    )
-}
